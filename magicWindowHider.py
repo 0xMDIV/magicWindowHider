f6f86570ee8d99008b750b8527e030b9ebc9887f
0xMDIV <0x.mdiv@gmail.com>
2020-04-22 22:39:18 +0200

    now its only 2 lines

--- magicWindowHider.py
+++ magicWindowHider.py
@@ -1,4 +1,2 @@
 import win32gui, win32con
-while True:
-	The_program_to_hide = win32gui.GetForegroundWindow()
-	win32gui.ShowWindow(The_program_to_hide , win32con.SW_HIDE)
+while True: win32gui.ShowWindow(win32gui.GetForegroundWindow() , win32con.SW_HIDE)
